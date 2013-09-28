5543562c9662c673ba9528e7ce1b9a2a2c25c21a
agamrat <amber.gamrat@mail.mcgill.ca>
2013-09-27 21:42:08 -0400

    first try

---
+++ Motsarella/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motsarella::Application.config.secret_token = '66b77992108d6369c54979d37cc818f762ff600448a87153928caa30fb4aa3f801433b64a43e506d5e5492d48a79bd5b479658205023a34877ddf041c0f99293'
