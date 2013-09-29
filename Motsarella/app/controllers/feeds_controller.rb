57b3419d0bc99a71421079ca95e916d578784688
jeffreyLeung01 <jeffrey.pyl@gmail.com>
2013-09-28 22:35:34 -0400

    BBC feed working

--- Motsarella/app/controllers/feeds_controller.rb
+++ Motsarella/app/controllers/feeds_controller.rb
@@ -3,25 +3,25 @@ require "simple-rss"
 require "rubygems"
 require 'nokogiri'
 #class FeedsController < ApplicationController
-	String topic = "fun"
-	theOnion = "http://feeds.theonion.com/theonion/daily"
-	bbc = "http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml"
-	reddit = "http://www.reddit.com/rss.xml" #["http://www.reddit.com/search.xml?q=RSS+Readers&sort=", topic].join("")
-	engadget = "www.engadget.com/rss.xml"
-	# stuff = open(theOnion).read
-	#puts open(bbc).read
-	rss = SimpleRSS.parse(
-		open(bbc)
-	 	)
-	#puts rss
-	theLink = rss.feed.link
-	puts theLink
-	# puts (
-	# 	open(theLink).read
-	# 	)
-	# rss.items.each do|i|
- #   	puts "Title: #{i.title}"
- #   	puts "Link: #{i.link}"
- #   	puts "=" * 80
+	#def feed
+		String topic = "fun"
+		theOnion = "http://feeds.theonion.com/theonion/daily"
+		bbc = "http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml"
+		reddit = "http://www.reddit.com/rss.xml" #["http://www.reddit.com/search.xml?q=RSS+Readers&sort=", topic].join("")
+		engadget = "www.engadget.com/rss.xml"
+	
+		rss = SimpleRSS.parse(
+			open(bbc)
+	 		)
+		#puts rss
+		theLink = rss.feed.link
+		puts theLink
+		page = Nokogiri::HTML(open(theLink))   
+		puts page.class 
+		mainCon = page.css('div#main-content')[0] 
+		res = mainCon.css('p').text
+		puts res
+	#end
+	
 
 #end
